17c84884c04ce2288001872c7db18f63c3efa55c
manikandan <manidhiyamech07167@gmail.com>
2022-09-30 21:29:05 +0530

    js math game

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var startEle=document.getElementById("starts");
 let display=document.getElementById("display");
 let done=document.getElementById("calculate");
-let incmt=0;
 
 function generateRandomnum(max){
     return parseInt(Math.random()*max + 1);
@@ -21,16 +20,39 @@ function getOperatorsym(operators){
     }
 }
 
+function generatedivisibleRandomnum1(max){
+    for(var i=0; i<550 ; i++){
+        let first=parseInt(Math.random()*(max/2) + 1);
+
+        if(first%2 == 0){
+            return first;
+        }
+        break;
+    }
+}
+function generatedivisibleRandomnum2(max){
+    for(var i=0; i<50 ; i++){
+        let first=parseInt(Math.random()*max + 1);
+
+        if(first%2 == 0 && (max/2)<first){
+            return first;
+        }
+        break;
+    }
+}
+
 function generateBox(randomNum1,randomNum2,operators){
 
     var col=document.createElement("div");
     col.setAttribute("class","col-3 mt-3");
     var card=document.createElement("div");
-    card.setAttribute("class","card");
-    card.setAttribute("id","list_"+incmt);
+    card.setAttribute("class","card section");
     var Number1=document.createElement("h3");
+    Number1.setAttribute("class","number1");
     var operatorsymbol=document.createElement("span");
+    operatorsymbol.setAttribute("class","operator")
     var Number2=document.createElement("h3");
+    Number2.setAttribute("class","number2");
     var calculateinput=document.createElement("input");
     calculateinput.setAttribute("type","number");
     calculateinput.setAttribute("class","answer");
@@ -51,68 +73,55 @@ function generateBox(randomNum1,randomNum2,operators){
 startEle.addEventListener("click",firstOperation);
 
 function firstOperation(){
+
     var operatorEle=document.getElementById("operators"),
         rangeEle=document.getElementById("range"),
         operators=operatorEle.value,
         range=rangeEle.value,
         randomNum1,randomNum2,total=[];
         display.innerHTML="";
-        if(operators !== ""){
+        if(operators !== "division"){
             for(var i=0 ;i < 16; i++){
                 randomNum1=generateRandomnum(range);
                 randomNum2=generateRandomnum(range);
                 
                 generateBox(randomNum1,randomNum2,operators);
-                if(operators == "add"){
-                    total.push((randomNum1+randomNum2).toString());
-                }
-                else if(operators == "sub"){
-                    total.push((randomNum1-randomNum2).toString());
-                }
-                else if(operators == "multiple"){
-                    total.push((randomNum1*randomNum2).toString());
-                }
-                else if(operators == "division"){
-                    total.push((randomNum1/randomNum2).toString());
-                }
-                incmt++;
-
             }
             
         } 
+        else{
+            for(var i=0 ;i < 16; i++){
+                randomNum1=generatedivisibleRandomnum1(range);
+                randomNum2=generatedivisibleRandomnum2(range);
+                console.log(randomNum1,randomNum2);
+                // generatedivisibleBox(randomNum1,randomNum2,operators);
+            }
+        }
         done.style.display="block";
-        done.addEventListener("click",function(){
-            var id="list_";
-            var arry=[];
-        for(var j=0; j<=15; j++){
-            var getid=id+j;
-            var parnt=document.getElementById(getid);
-            var inputValue=parnt.getElementsByClassName("answer")[0];
-            var ans=inputValue.value;
-            arry.push(ans.toString());
-        }
-        
-        console.log(arry);
-        console.log(total);
-        var crtAns=0;
-        var wrongAns=0;
-        var stage2="list_";
-        for(var l=0; l<=15; l++){
-            var getstage2id=id+l;
-            var cardclr=document.getElementById(getstage2id);
-            if(arry[l] == total[l]){
-                crtAns++;
-                cardclr.style.background="green";
-            }
-            else{
-                wrongAns++;
-                cardclr.style.background="red";
-            }
-        }
-        console.log(crtAns);
-        console.log(wrongAns);
-        done.style.display="none";
-        
-        })
 }
-
+done.addEventListener("click",function(){
+    let number1Ele,number2Ele,operator,total;
+    for(var i=0; i<16; i++){
+        let getId=document.getElementsByClassName("section")[i];
+        number1Ele=getId.getElementsByClassName("number1")[0].innerText;
+        number2Ele=getId.getElementsByClassName("number2")[0].innerText;
+        operator=getId.getElementsByClassName("operator")[0].innerText;
+        if(operator == "+"){
+            total=parseInt(number1Ele) + parseInt(number2Ele);
+        }
+        else if(operator == "-"){
+            total=parseInt(number1Ele) - parseInt(number2Ele);
+        }
+        else if(operator == "*"){
+            total=parseInt(number1Ele) * parseInt(number2Ele);
+        }
+        let inpAns=getId.getElementsByClassName("answer")[0].value;
+        
+        if(total == inpAns){
+            getId.style.background="green";
+        }
+        else{
+            getId.style.background="red";
+        }
+    }
+})
